e4939fc6f3b3bbce04ddf64fd25a0efa3e81dbc4
villadalmine <rino@atadata.com>
2017-03-20 18:14:42 -0300

    added first try

---
+++ print_bot_id.py
@@ -0,0 +1,20 @@
+import os
+from slackclient import SlackClient
+
+
+BOT_NAME = 'viola'
+
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+#slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+#slack_client = 'xoxb-156878149187-OPZCDKVupxclxLprsbieBY96'
+print ( slack_client)
+if __name__ == "__main__":
+    api_call = slack_client.api_call("users.list")
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+    else:
+        print("could not find bot user with the name " + BOT_NAME)
